ce5b12557cc7dfcfd766791f273071e689863ab1
Pwnkdm <pawanrajkadam@gmail.com>
2022-07-20 21:51:58 +0530

    topsection of package page done

--- src/pages/Package.jsx
+++ src/pages/Package.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import TopInputBox from "../components/TopInputBox";
 
 const Package = () => {
-  return <div>Package</div>;
+  return (
+    <div>
+      <TopInputBox />
+    </div>
+  );
 };
 
 export default Package;
