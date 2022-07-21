e29bd5fa22c5e405d07352a7b743cb708914e748
AnwardeenM <kalilanwar597@gmail.com>
2022-07-21 07:42:25 +0530

    working on Navbar&Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Package from "./pages/Package";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Package />
+      <Footer />
     </div>
   );
 }
