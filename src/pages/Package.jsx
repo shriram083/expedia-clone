ca58fe91cdb49085d4d6f9a975474b410aee3840
Pwnkdm <pawanrajkadam@gmail.com>
2022-07-21 22:17:17 +0530

    Package sidebar completed

--- src/pages/Package.jsx
+++ src/pages/Package.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import PackageSidebar from "../components/PackageSidebar";
 import TopInputBox from "../components/TopInputBox";
 
 const Package = () => {
   return (
     <div>
       <TopInputBox />
+      <PackageSidebar />
     </div>
   );
 };
