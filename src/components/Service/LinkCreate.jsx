b76dc36b99bc458bcb8585f1e0214540be7c654f
Rohit Bhaud <92250506+RohitBhaud@users.noreply.github.com>
2022-07-22 22:24:04 +0530

    Bottom

---
+++ src/components/Service/LinkCreate.jsx
@@ -0,0 +1,10 @@
+import { Link } from '@chakra-ui/react'
+import React from 'react'
+
+const LinkCreate = ({title}) => {
+  return (
+    <Link style={{marginRight:"15px"}}>{title}</Link>
+  )
+}
+
+export default LinkCreate
