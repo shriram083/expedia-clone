44191927f4811af038c2bcd972ba496ce92f74c4
Pwnkdm <pawanrajkadam@gmail.com>
2022-07-22 22:28:36 +0530

    hotel list body comleted

---
+++ src/redux/app/appTypes.js
@@ -0,0 +1,4 @@
+// hotel list variables
+export const GET_HOTELS_REQUEST = "GET_HOTELS_REQUEST";
+export const GET_HOTELS_SUCCESS = "GET_HOTELS_SUCCESS";
+export const GET_HOTELS_FAILED = "GET_HOTELS_FAILED";
